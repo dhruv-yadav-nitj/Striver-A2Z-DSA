7c9e80861bc0c4d5cf60c1a7888a9b47978070ac
Dhruv <dhruv.nitj@gmail.com>
2024-09-14 00:45:24 +0530

    added OOP

---
+++ Step 20 - OOP/ABSTRACTION.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+    @abstractmethod
+    def start(self):
+        pass
+    @abstractmethod
+    def stop(self):
+        pass
+
+class Car(Vehicle):
+    def start(self):
+        print("Car is starting")
+    def stop(self):
+        print("Car is applying brakes")
+    
